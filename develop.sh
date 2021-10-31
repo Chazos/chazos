38fcdc18968de3526765ac36657a5343d16e3c5e
Takunda Madechangu <madechangu.takunda@gmail.com>
2021-10-31 20:29:00 +0200

    🔥  Remove redundant files

--- develop.sh
+++
@@ -1,6 +0,0 @@
-mv index.php public/index.php
-sed -i "s#__DIR__.'/#__DIR__.'/../#g" public/index.php
-sed -i "s#env('ASSET_URL', 'public/')#env('ASSET_URL', null)#g"  config/app.php
-sed -i "s#APP_DEBUG=false#APP_DEBUG=true#g"  .env
-sed -i "s#APP_ENV=production#APP_ENV=local#g" .env
-echo "👨🏽‍💻 Now in develop mode"
--- install.bat
+++
@@ -1,5 +0,0 @@
-composer update
-php artisan config:cache
-php artisan migrate
-php artisan db:seed
-php artisan storage:link

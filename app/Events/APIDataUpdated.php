9ea082c606c9b00c2f5bdf0966dd51a0ff9024e2
Takunda Madechangu <madechangu.takunda@gmail.com>
2021-11-16 14:46:32 +0200

    Created events and add some data

---
+++ app/Events/APIDataUpdated.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Events;
+
+use Illuminate\Broadcasting\Channel;
+use Illuminate\Broadcasting\InteractsWithSockets;
+use Illuminate\Broadcasting\PresenceChannel;
+use Illuminate\Broadcasting\PrivateChannel;
+use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
+use Illuminate\Foundation\Events\Dispatchable;
+use Illuminate\Queue\SerializesModels;
+
+class APIDataUpdated
+{
+    use Dispatchable, InteractsWithSockets, SerializesModels;
+
+    protected $data;
+    protected $table;
+
+    /**
+     * Create a new event instance.
+     *
+     * @return void
+     */
+    public function __construct($table, $data)
+    {
+        $this->table = $table;
+        $this->data = $data;
+    }
+
+    /**
+     * Get the channels the event should broadcast on.
+     *
+     * @return \Illuminate\Broadcasting\Channel|array
+     */
+    public function broadcastOn()
+    {
+        return new PrivateChannel('channel-name');
+    }
+}

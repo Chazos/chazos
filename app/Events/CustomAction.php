6916fa4c53f4e66bdcb89ce84c92a7e05884c016
Takunda Madechangu <madechangu.takunda@gmail.com>
2021-11-20 03:35:28 +0200

    Add a custom action action event

---
+++ app/Events/CustomAction.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Events;
+
+use Illuminate\Broadcasting\Channel;
+use Illuminate\Broadcasting\InteractsWithSockets;
+use Illuminate\Broadcasting\PresenceChannel;
+use Illuminate\Broadcasting\PrivateChannel;
+use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
+use Illuminate\Foundation\Events\Dispatchable;
+use Illuminate\Queue\SerializesModels;
+
+class CustomAction
+{
+    use Dispatchable, InteractsWithSockets, SerializesModels;
+
+    protected $action;
+    protected $table;
+    protected $id;
+
+    /**
+     * Create a new event instance.
+     *
+     * @return void
+     */
+    public function __construct($action,$table, $id)
+    {
+        $this->action = $action;
+        $this->table = $table;
+        $this->id = $id;
+    }
+
+    /**
+     * Get the channels the event should broadcast on.
+     *
+     * @return \Illuminate\Broadcasting\Channel|array
+     */
+    public function broadcastOn()
+    {
+        return new PrivateChannel('channel-name');
+    }
+}

6cd159093c60ba13eeb774517a9a0241b7b660b3
Takunda Madechangu <madechangu.takunda@gmail.com>
2021-12-13 12:17:19 +0200

    Added example plugins

---
+++ examples/plugins/Chazol/Chazol.php
@@ -0,0 +1,18 @@
+<?php
+
+$plugin_info = array(
+    'name' => 'Chazol',
+    'version' => '1.0',
+    'author' => 'Takunda Madechangu',
+    'plugin_url' => 'http://www.azol.com.au',
+    'author_url' => 'http://www.azol.com.au',
+    'description' => 'Plugin to allow the Media Manager to add files from the webservers filesystem.',
+    'installed' => true,
+    'plugin_options' => [
+        'settings' => 'http://www.meow.com',
+
+    ]
+);
+
+
+

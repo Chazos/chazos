6d55d8d0c9214172238155b6d791ba09f572d76b
Takunda Madechangu <madechangu.takunda@gmail.com>
2021-12-13 12:17:49 +0200

    Add transactions model, controller, migration

---
+++ database/migrations/2021_12_13_094518_create_transactions_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTransactionsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('transactions', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('user_id');
+            $table->unsignedBigInteger('order_id');
+            $table->decimal('total', 10, 2);
+            $table->decimal('tax', 10, 2);
+            $table->decimal('discount', 10, 2);
+            $table->decimal('grand_total', 10, 2);
+            $table->string('currency');
+            $table->string('gateway');
+            $table->boolean('paid')->default(false);
+            $table->boolean('refunded')->default(false);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('transactions');
+    }
+}

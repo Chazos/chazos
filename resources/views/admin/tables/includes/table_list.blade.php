262f346c47b38d3b941a1a2e8df5994c5bb6ee2e
Takunda Madechangu <madechangu.takunda@gmail.com>
2021-12-15 13:16:23 +0200

    Removed uneeded code

--- resources/views/admin/tables/includes/table_list.blade.php
+++ resources/views/admin/tables/includes/table_list.blade.php
@@ -6,13 +6,13 @@
 
         <ul>
             @foreach ( $tables as $table )
-                <li  class="py-3 hover:bg-purple-400 hover:text-white rounded-full mr-3 px-3 flex flex-row">
+                <li  class="py-3 hover:bg-purple-400 hover:text-white rounded-full mr-3 px-3 flex justify-between">
                   <button id="tbl-item-{{ $table->id }}" x-on:click="getTableDetails({{ $table->id }}); showCollectionTable = true; showAddField = false; showCollectionNameForm =true" class="">{{ ucfirst($table->display_name)  }}</button>
 
 
                   @if ($table->table_name != 'users')
-                    <span class="w-full"></span>
-                    <button class="mr-3" type="button" x-on:click="deleteCollection({{ $table->id }})">
+
+                    <button class="" type="button" x-on:click="deleteCollection({{ $table->id }})">
                         <svg class="w-6 h-6 text-red-600" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16"></path></svg>
                     </button>
 

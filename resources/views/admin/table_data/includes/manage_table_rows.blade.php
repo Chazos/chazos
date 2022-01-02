6e4916da2997061633ad7f52351b23b58dc08135
Takunda Madechangu <madechangu.takunda@gmail.com>
2022-01-02 12:07:35 +0200

    Added view to actions

--- resources/views/admin/table_data/includes/manage_table_rows.blade.php
+++ resources/views/admin/table_data/includes/manage_table_rows.blade.php
@@ -56,6 +56,15 @@
 
                         <hr class="border-gray-200 dark:border-gray-700 ">
                         <a
+                            href="{{ route('admin.view_item', ['table_name' => $table->table_name, 'id' => $item->id]) }}"
+                            class="flex w-full items-center p-3 text-sm text-gray-600 capitalize transition-colors duration-200 transform dark:text-gray-300 hover:bg-gray-100 dark:hover:bg-gray-700 dark:hover:text-white">
+                            <svg class="w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 12a3 3 0 11-6 0 3 3 0 016 0z"></path><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M2.458 12C3.732 7.943 7.523 5 12 5c4.478 0 8.268 2.943 9.542 7-1.274 4.057-5.064 7-9.542 7-4.477 0-8.268-2.943-9.542-7z"></path></svg>
+
+                            <span class="mx-1">
+                                View
+                            </span>
+                        </a>
+                        <a
                             href="{{ route('admin.edit_item', ['table_name' => $table->table_name, 'id' => $item->id]) }}"
                             class="flex w-full items-center p-3 text-sm text-gray-600 capitalize transition-colors duration-200 transform dark:text-gray-300 hover:bg-gray-100 dark:hover:bg-gray-700 dark:hover:text-white">
                             <svg class="w-5 h-5" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20">

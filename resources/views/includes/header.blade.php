33b1bdcc666b893af6db2b3847ec1d1d80623068
Takunda Madechangu <madechangu.takunda@gmail.com>
2022-02-28 13:17:07 +0200

    Remove "Search for projects"

--- resources/views/includes/header.blade.php
+++ resources/views/includes/header.blade.php
@@ -11,7 +11,7 @@
             </svg>
         </button>
         <!-- Search input -->
-        <div class="flex justify-center flex-1 lg:mr-32">
+        {{-- <div class="flex justify-center flex-1 lg:mr-32">
             <div class="relative w-full max-w-xl mr-6 focus-within:text-purple-500">
                 <div class="absolute inset-y-0 flex items-center pl-2">
                     <svg class="w-4 h-4" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20">
@@ -24,8 +24,8 @@
                     class="w-full pl-8 pr-2 text-sm text-gray-700 placeholder-gray-600 bg-gray-100 border-0 rounded-md dark:placeholder-gray-500 dark:focus:shadow-outline-gray dark:focus:placeholder-gray-600 dark:bg-gray-700 dark:text-gray-200 focus:placeholder-gray-500 focus:bg-white focus:border-purple-300 focus:outline-none focus:shadow-outline-purple form-input"
                     type="text" placeholder="Search for projects" aria-label="Search" />
             </div>
-        </div>
-        <ul class="flex items-center flex-shrink-0 space-x-6">
+        </div> --}}
+        <ul class="flex items-center ml-auto flex-shrink-0 space-x-6">
             <!-- Theme toggler -->
             {{-- <li class="flex">
                 <button class="rounded-md focus:outline-none focus:shadow-outline-purple"
